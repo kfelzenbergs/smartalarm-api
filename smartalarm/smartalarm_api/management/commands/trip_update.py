7d087492ad8be0278687ad892decc3ae1d73829a
Kristaps Felzenbergs <kristaps.felzenbergs@testdevlab.com>
2017-11-09 15:14:03 +0200

    disabling check for altitude changes on trips

--- smartalarm/smartalarm_api/management/commands/trip_update.py
+++ smartalarm/smartalarm_api/management/commands/trip_update.py
@@ -8,14 +8,14 @@ class Command(BaseCommand):
             diff = 0.001
             lat_diff = 0
             lon_diff = 0
-            alt_diff = 0
+            # alt_diff = 0
 
             for i in range(0, len(stats)-1):
                 lat_diff += abs(stats[i].stats.lat - stats[i+1].stats.lat)
                 lon_diff += abs(stats[i].stats.lon - stats[i+1].stats.lon)
-                alt_diff += abs(stats[i].stats.alt - stats[i+1].stats.alt)
+                # alt_diff += abs(stats[i].stats.alt - stats[i+1].stats.alt)
     
-            if lat_diff > diff or lon_diff > diff or alt_diff > diff:
+            if lat_diff > diff or lon_diff > diff:
                 return True
             else:
                 return False
